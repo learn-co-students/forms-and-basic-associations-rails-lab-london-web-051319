67fee2a062a4cc8f443932a94715274f38b16226
moniet <moniet.sawhney@gmail.com>
2019-06-10 18:02:12 +0100

    added artist solution

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,21 +14,25 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(inp)
-    self.artist = Artist.find_or_create_by(name: inp)
+    artist = Artist.find_or_create_by(name: inp)
+    artist.songs.build(name: self.name)
+    binding.pry
   end
 
   def artist_name
     self.artist.name
+    # binding.pry
   end
 
-  def note_contents=(notes_arr)
-    notes = Note.create(content: notes_arr)
-    self.notes << notes
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
   end
 
   def note_contents
-    val = self.notes
-    val.content
-
+    self.notes.map(&:content)
   end
 end
